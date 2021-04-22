0151be180b851f9c4219ecdde8cdc524f335ac83
ChinoAladino <chinoaladino@hotmail.com>
2021-04-22 16:00:55 -0400

    add api and card
    4 commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-$(document).ready(function() {
-    $.get("https://192.168.1.89:3000/product",function (data, textStatus, jqXHR) {
-            console.log(data)
-            console.log(textStatus)
-            console.log(jqXHR)
-        },
-        "dataType"
-    );
-   
-})
+const container = document.getElementById('container-card');
+
+const obtenerProductos = async () => {
+    try {
+        const res = await fetch('http://127.0.0.1:3000/product');
+        const data = await res.json();
+        console.log(data)
+        data.forEach(data => {
+            container.innerHTML += `
+            <div class="card col-md-6 col-sm-12 col-lg-4 mt-2" style="height: 10%; width="200px;">
+                <img class="card-img-top" src=${data.url_image} />
+                <h5 class="card-title text-center">${data.name}</h5>
+                <div class="card-footer">
+                   <div class="row">
+                        <div class="col col-md-6">
+                            <div class="btn btn-danger">
+                                precio
+                            </div>
+                        </div>
+                   </div>
+                </div>
+            </div>
+            `
+        });
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+obtenerProductos();
